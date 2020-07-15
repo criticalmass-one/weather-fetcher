94bb2057e1d43eef4d9bd78a99de42dc3d8bc552
Malte Hübner <maltehuebner@gmx.org>
2020-07-15 02:09:41 +0200

    Remove old stuff.

--- src/ForecastManager/ForecastManagerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace App\ForecastManager;
-
-interface ForecastManagerInterface
-{
-    public function retrieve(\DateTime $startDateTime = null, \DateTime $endDateTime = null): array;
-    public function getNewWeatherForecasts(): array;
-}
