1a73c5c1a0a7f49447ca7f62fd82f4e5292a2de9
Malte Hübner <maltehuebner@gmx.org>
2020-08-29 09:07:38 +0200

    Do not check ssl certificates as they are self signed.

--- src/RideRetriever/RideRetriever.php
+++ src/RideRetriever/RideRetriever.php
@@ -14,6 +14,7 @@ class RideRetriever implements RideRetrieverInterface
     {
         $this->client = new Client([
             'base_uri' => $criticalmassHostname,
+            'verify' => false,
         ]);
 
         $this->serializer = $serializer;
